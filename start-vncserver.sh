b549130f3bb3794dc0d0966500983e7c9d5eec65
George <developer613@sourceplay.com>
2020-03-26 22:55:21 +0700

    for selenium grid v4

--- start-vncserver.sh
+++ start-vncserver.sh
@@ -3,4 +3,5 @@
 echo "starting VNC server ..."
 export USER=root
 alias python=python3
+java -jar baangt/browserDrivers/selenium-server-4.0.0-alpha-5.jar standalone &
 vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log
